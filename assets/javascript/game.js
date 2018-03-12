b0c11214f6d9be7d70585494a1ac79b733e79bdf
Jonathan Disla <jon.disla@gmail.com>
2018-03-11 21:38:10 -0400

    fixing around

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -91,18 +91,50 @@
 //The value of each crystal is hidden from you until you click on it
 //Each time the game starts, the game will change the values of each crystal
 
-var numbers = [1,2,3,4,5];
 
-document.onclick = randNums;
+$( document ).ready(function() {
+    console.log( "ready!" );
+var ramResult;
+var win;
+var lost;
+
+
+$('.btn').click(function() {
+    ramResult = Math.floor(Math.random() * 50 + 1);
+    $('.btn').html(ramResult)
+
+})
+
+// document.onclick = randNums;
 function randNums(){
-    for (var i = 4; i < numbers.length; i++){
-    var random = Math.floor(Math.random() * 10);
-        // console.log(random1)
-    $('#crystals').on('click', function(){
-        console.log(random)
-    
-        })
-    }
-}
+    for (var i = 0; i < 1; i++){
 
-randNums()
+    var random = Math.floor(Math.random() * 5 + 1);
+    console.log(random)
+
+    var random2 = Math.floor(Math.random() * 12 + 1);
+    console.log(random2)
+
+    var random3 = Math.floor(Math.random() * 8 + 1);
+    console.log(random3)    
+
+    var random4 = Math.floor(Math.random() * 3 + 1);
+    console.log(random4)
+    
+    $('#crystal1').on("click", function(){
+        console.log($(this).attr(random2))
+    })
+    $('#crystal2').on("click", function(){
+        console.log($(this))
+    })
+    $('#crystal3').on("click", function(){
+        console.log($(this))
+    })
+    $('#crystal4').on("click", function(){
+        console.log($(this))
+    })
+
+
+        }
+    }randNums()
+})
