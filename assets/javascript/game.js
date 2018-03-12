33e3f49babf9e4b37cbc6f53aa778a0d26cf21c2
Jonathan Disla <jon.disla@gmail.com>
2018-03-11 22:51:00 -0400

    commit last

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
 var ramResult;
 var win;
 var lost;
+var adding = 0;
 
 
 $('.btn').click(function() {
@@ -16,31 +17,43 @@ function randNums(){
     for (var i = 0; i < 1; i++){
 
     var random = Math.floor(Math.random() * 5 + 1);
+    var r1 = random.toString().toLowerCase();
     console.log(random)
+    var r2 = random.toString().toLowerCase();
+    
 
     var random2 = Math.floor(Math.random() * 12 + 1);
     console.log(random2)
+    var r2 = random2.toString().toLowerCase();
 
     var random3 = Math.floor(Math.random() * 8 + 1);
-    console.log(random3)    
+    console.log(random3) 
+    var r3 = random3.toString().toLowerCase();   
 
     var random4 = Math.floor(Math.random() * 3 + 1);
     console.log(random4)
+    var r4 = random4.toString().toLowerCase();
     
-    $('#crystal1').on("click", function(){
-        console.log($(this).attr(random2))
+    $('#crystals').on("click", function(){
+        $('#points').attr(r1);
+        $('#points').attr(r2);
+        adding += parseInt(r1, r2)
+        console.log(adding)
     })
     $('#crystal2').on("click", function(){
-        console.log($(this))
+        $('#points').attr(r2);
     })
     $('#crystal3').on("click", function(){
-        console.log($(this))
+        $('#points').attr(r3);
     })
     $('#crystal4').on("click", function(){
-        console.log($(this))
+        $('#points').attr(r4);
     })
-
+    // adding += parseInt(r1) + parseInt(r2) + parseInt(r3) + parseInt(r4);
+    // console.log(adding)
 
         }
-    }randNums()
+    
+    }
+    randNums()
 })
